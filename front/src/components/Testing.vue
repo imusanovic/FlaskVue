f33365c780410e2bfa00ec89080add11fb2a19a0
Ivan <imusanovic@gmail.com>
2019-05-28 11:11:35 +0200

    Start app

---
+++ front/src/components/Testing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="container">
+        <h1>{{ data.ime }} {{ data.prezime }}</h1>
+        <h2>Adresa: {{ data.adresa.ulica }} {{ data.adresa.broj }}, {{ data.adresa.grad }}</h2>
+        <h2>Mobitel: {{ data.mobitel }}</h2>
+    </div>
+  </div>
+</template>
+
+<style>
+    #app{
+        margin-top:60px;
+    }
+</style>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Test',
+  data() {
+    return {
+      data: '',
+    };
+  },
+  methods: {
+    testing() {
+        const endpoint = '//localhost:5000/test'
+        axios.get(endpoint).then((res) => {
+            this.data = res.data;
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    }
+  },
+  created(){
+    this.testing();
+  }
+};
+</script>
